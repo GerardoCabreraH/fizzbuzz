aaf281c5fb2471dbc958fc0583e599506e7bb6ab
Gerardo Rafael Cabrera Herrera <gecabrera@outlook.com>
2022-04-29 21:40:38 -0500

    Optimizacion de fizzbuzz

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -31,34 +31,22 @@ describe("Test para ExplorerController", () => {
         expect(explorersInJava).toBe(5);
     });
 
-    test("Requerimiento 7: Obtener el validador de explorers por node", () => {
-        const explorersInNode = ExplorerController.getExplorersByMission("node");
-        const getvalidatorInNode = explorersInNode.map((explorer) => ExplorerController.getValidationInExplorer(explorer));
-        expect(getvalidatorInNode.length).toBe(10);
-    });
-
-    test("Requerimiento 8: Obtener el validador tricks de explorers por java", () => {
-        const explorersInJava = ExplorerController.getExplorersByMission("java");
-        const getvalidatorInJava = explorersInJava.map((explorer) => ExplorerController.getValidationInExplorer(explorer));
-        expect(getvalidatorInJava.length).toBe(5);
-    });
-
-    test("Requerimiento 9: Obtener el validador score", () => {
+    test("Requerimiento 7: Obtener el validador score", () => {
         const getvalidatorInScore = ExplorerController.getValidationInScore(1);
         expect(getvalidatorInScore).toBe(1);
     });
 
-    test("Requerimiento 10: Obtener el validador FIZZ", () => {
+    test("Requerimiento 8: Obtener el validador FIZZ", () => {
         const getvalidatorInScore = ExplorerController.getValidationInScore(3);
         expect(getvalidatorInScore).toBe("FIZZ");
     });
 
-    test("Requerimiento 11: Obtener el validador BUZZ", () => {
+    test("Requerimiento 9: Obtener el validador BUZZ", () => {
         const getvalidatorInScore = ExplorerController.getValidationInScore(5);
         expect(getvalidatorInScore).toBe("BUZZ");
     });
 
-    test("Requerimiento 12: Obtener el validador FIZZBUZZ", () => {
+    test("Requerimiento 10: Obtener el validador FIZZBUZZ", () => {
         const getvalidatorInScore = ExplorerController.getValidationInScore(15);
         expect(getvalidatorInScore).toBe("FIZZBUZZ");
     });
